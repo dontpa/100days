0c4b401b58bb8f743f7933d083c954ea4a4c9fbf
dontpa <dontpa@icloud.com>
2024-08-21 20:39:45 +0800

    Initial Commit

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+var greeting :String = "Hello, playground"
+print(greeting)
+
+let movie = "a movie name"
+print(movie.count)
+
+print(movie.uppercased())
+print(movie)
+print(movie.hasPrefix("a"))
+
+let multilinestr = """
+a multiline string "
+another line
+"""
+print(multilinestr)
+
