f9a3d13e7a28f5c43b16782b83b1b226e57d6f54
dontpa <dontpa@icloud.com>
2024-08-21 21:09:13 +0800

    day 1&2

--- day1.playground/Contents.swift
+++ day1.playground/Contents.swift
@@ -16,3 +16,20 @@ another line
 """
 print(multilinestr)
 
+let intnumber = 100_000_000
+print(intnumber.isMultiple(of: 50))
+print(intnumber/3)
+
+let number = 0.1 + 0.2
+print(number)
+
+var aboolean = false
+print(aboolean)
+aboolean.toggle()
+print(aboolean)
+
+
+// checkpoint 1
+let celsius : Double = 38
+let calculated = celsius * 9 / 5 + 32
+print("the Fahrenheit temperature is \(calculated)°")
