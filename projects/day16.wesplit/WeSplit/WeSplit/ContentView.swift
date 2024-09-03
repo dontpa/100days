a8101698e6ed0b70ea5e9f93d8a4c682fa7eeb21
dontpa <dontpa@icloud.com>
2024-09-03 22:02:05 +0800

    day 16&17

---
+++ projects/day16.wesplit/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by mrw on 2024/9/3.
+//
+
+import SwiftUI
+
+//struct ContentView: View {
+//    @State private var name = ""
+//    let students = ["Harry" ,"Hermione", "Ron"]
+//    @State private var selectedStudent = "Harry"
+//    
+//    var body:some View{
+//        NavigationStack{
+//            Form{
+//                Picker("Select your student", selection: $selectedStudent){
+//                    ForEach(students,id:\.self){
+//                        Text($0)
+//                    }
+//                    
+//                }
+//                Text("selected name =  \(selectedStudent)")
+//                
+//                ForEach(0..<100){
+//                    Text("Row \($0)")
+//                    
+//                }
+//                TextField("Enter your name", text: $name)
+//                Text("your name is \(name)")
+//            }
+//        }
+//    }
+//}
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        (checkAmount / 100 * Double(tipPercentage) + checkAmount) / Double(numberOfPeople + 2)
+    }
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                }
+                Picker("Number of people,", selection: $numberOfPeople) {
+                    ForEach(2..<100) {
+                        Text("\($0) people")
+                    }
+                }
+                .pickerStyle(.navigationLink)
+                Section("How much tip do you want to leave?") {
+                    
+                    Picker("Tip percentage: ", selection: $tipPercentage){
+                        ForEach(tipPercentages,id:\.self){
+                            Text($0, format: .percent)
+                        }
+                    } .pickerStyle(.segmented)
+                }
+                Section{
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+
+               
+            }
+            .navigationTitle("WeSplit")
+            .toolbar{
+                if amountIsFocused{
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ projects/day16.wesplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 王晨 on 2024/9/3.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
